2da2bfb0dae70d527de75d6e9e29a3a2368560ba
Memo <memosoft@ya.ru>
2024-01-31 12:15:42 +0300

    feat: extra scripts for develop proccess

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,16 +1,24 @@
-import { rollupPluginHTML as html } from "@web/rollup-plugin-html";
-import copy from "rollup-plugin-copy";
+import { rollupPluginHTML as html } from '@web/rollup-plugin-html'
+import copy from 'rollup-plugin-copy'
 
 export default {
-  input: "src/index.html",
-  output: { dir: "dist" },
+  input: 'src/index.html',
+  output: { dir: 'dist' },
   plugins: [
-    html(),
+    html({
+      minify: true,
+      htmlMinifierOptions: {
+        collapseWhitespace: true,
+        collapseBooleanAttributes: true,
+        conservativeCollapse: true,
+        minifyJS: true,
+      },
+    }),
     copy({
       targets: [
-        { src: "src/images/**/*", dest: "dist/images" },
-        { src: "src/sounds/**/*", dest: "dist/sounds" },
+        { src: 'src/images/**/*', dest: 'dist/images' },
+        { src: 'src/sounds/**/*', dest: 'dist/sounds' },
       ],
     }),
   ],
-};
+}
